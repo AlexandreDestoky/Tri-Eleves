fd8826fbe66dead53c3270f14150820ad5d4d4f4
Destoky <adestoky@gmail.com>
2020-11-12 18:54:47 +0100

    avancement

---
+++ app.js
@@ -0,0 +1,38 @@
+let eleves = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+let atelier = ["Lego", "PlayMobil", "ClicsToys", "Bloc Papi", "Lecture", "Mandala"];
+let groupe = [[], [], [], [], [], []];
+let groupe1 = [[], [], [], [], [], []];
+
+// //ajout de chaque élève
+// for (const eleve of eleves) {
+//   if (groupe[0].length < 3) {
+//     groupe[0].push(eleve);
+//   } else if (groupe[1].length < 3) {
+//     groupe[1].push(eleve);
+//   } else if (groupe[2].length < 3) {
+//     groupe[2].push(eleve);
+//   } else if (groupe[3].length < 3) {
+//     groupe[3].push(eleve);
+//   } else if (groupe[4].length < 3) {
+//     groupe[4].push(eleve);
+//   } else if (groupe[5].length < 3) {
+//     groupe[5].push(eleve);
+//   }
+// }
+// //ajout de chaque élève
+// for (const eleve of eleves) {
+//   if (groupe1[0].length < 3 && groupe[0].includes(eleve) == false ) {
+//     groupe1[0].push(eleve);
+//   } else if (groupe1[1].length < 3) {
+//     groupe1[1].push(eleve);
+//   } else if (groupe1[2].length < 3) {
+//     groupe1[2].push(eleve);
+//   } else if (groupe1[3].length < 3) {
+//     groupe1[3].push(eleve);
+//   } else if (groupe1[4].length < 3) {
+//     groupe1[4].push(eleve);
+//   } else if (groupe1[5].length < 3) {
+//     groupe1[5].push(eleve);
+//   }
+// }
+// console.log(groupe);
